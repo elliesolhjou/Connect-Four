6ac27605539fefb4b48e194098f1bf734e3ac130
Ellie Solhjou <elliesolhjou@gmail.com>
2024-06-02 15:58:54 -0500

    set up access grid and rows and cols count

--- backendcode.py
+++ backendcode.py
@@ -7,6 +7,7 @@ class GridPosition(enum.Enum):
 
 # Grid class to maintain the state of the 2-D board
 class Grid:
+    # initialize
     def __init__(self, rows, cols):
         # "_" defines for internal use and encapsulation
         self._rows = rows
@@ -14,6 +15,17 @@ class Grid:
         self._grid = None
         self.initGrid()
 
+    # methods:
+    # create 2D grid (list of lists)
     def initGrid(self):
-        # create 2D grid (list of lists)
         self._grid = [[GridPosition.EMPTY for i in range(self._cols)]for i in range(self._rows)]
+
+    # access the grid
+    def getGrid(self):
+        return self._grid
+    
+    def getColumnsCount(self):
+        return self._cols
+    
+    def getRowsCount(self):
+        return self._rows
