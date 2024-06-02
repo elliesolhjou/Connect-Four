5b884d5d1eac09f5f5a2f25566872682debbec00
Ellie Solhjou <elliesolhjou@gmail.com>
2024-06-02 16:06:10 -0500

    set up place token in grid unction

--- backendcode.py
+++ backendcode.py
@@ -6,6 +6,7 @@ class GridPosition(enum.Enum):
     RED = 2
 
 # Grid class to maintain the state of the 2-D board
+# this class helps us to see if a cell is available or not grid[row][col] ==1
 class Grid:
     # initialize
     def __init__(self, rows, cols):
@@ -24,8 +25,24 @@ class Grid:
     def getGrid(self):
         return self._grid
     
+    # access the grid sizes
     def getColumnsCount(self):
         return self._cols
     
+    # access the grid sizes
     def getRowsCount(self):
         return self._rows
+    
+    # place tokens -> asks about columns bc drop token takes all the way 
+    # down to the available cell
+    def placePieces(self, column, piece):
+        if column <0 or column >= self._cols:
+            raise ValueError("Selected Column Doesn't Exist")
+        if piece == GridPosition.EMPTY:
+            raise ValueError("Invalid token, Please pick a yellow or red token")
+        for row in range(self._rows-1, -1, -1):
+            if self._grid[row][column] == GridPosition.EMPTY:
+                self._grid[row][column] = piece
+                return row
+            
+    # 
